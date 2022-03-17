82241ab08230277ade009d392807ed0971bb2407
Jeezman <tobiadeyemi91@gmail.com>
2022-03-17 09:36:13 +0100

    Handle show sidebar on mobile

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Sidebar } from './components/Sidebar'
+import { MenuIcon } from "@heroicons/react/outline";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const setter = set => e => {
+    const { target } = e;
+    const { value } = target;
+    set(value);
+  };
+
   return (
     <Router>
-      <div className="App">
-        <Sidebar setSidebarOpen={() => { }} sidebarOpen={true} />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className='text-3xl font-bold underline'>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <Sidebar setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen} />
+      <div className="md:pl-64 flex flex-col flex-1">
+        <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
+          <button
+            type="button"
+            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            onClick={() => setSidebarOpen(true)}
           >
-            Learn React
-          </a>
-        </header>
+            <span className="sr-only">Open sidebar</span>
+            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
       </div>
     </Router>
   );
